c1e483c8fe8ea9b317d680440c2e6381244787e6
Oleksandr Peresunko <Oleksandr.Peresunko@fglsports.com>
2025-09-05 15:51:13 +0300

    Fixed output description

--- task05/outputs.tf
+++ task05/outputs.tf
@@ -1,3 +1,4 @@
 output "traffic_manager_fqdn" {
-  value = module.traffic_manager.traffic_manager_profile_fqdn
+  value       = module.traffic_manager.traffic_manager_profile_fqdn
+  description = "The fully qualified domain name (FQDN) of the Traffic Manager profile."
 }
